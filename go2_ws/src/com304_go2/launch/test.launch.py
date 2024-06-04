6ddf2b05fc179a13baa74ad596cba4e3160716a3
Hod Kimhi <hodkimhi@gmail.com>
2024-06-04 20:27:04 +0200

    cleanup launch file

--- go2_ws/src/com304_go2/launch/test.launch.py
+++ go2_ws/src/com304_go2/launch/test.launch.py
@@ -1,23 +1,19 @@
-import os
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    robot_ip = LaunchConfiguration('robot_ip', default=os.getenv('ROBOT_IP'))
-    robot_token = LaunchConfiguration('robot_token', default=os.getenv('ROBOT_TOKEN',''))
 
+def generate_launch_description():
     return LaunchDescription([
         Node(
             package='com304_go2',
             executable='go2_control_node',
-            ),
+        ),
         Node(
             package='com304_go2',
             executable='go2_camera_node',
-            ),
+        ),
         Node(
             package='com304_go2',
             executable='go2_depth_node',
-            ),
+        ),
     ])
